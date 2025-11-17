3059b125bc30d10cca6a2ba6a428119c3200c0ab
bajricdin <din.bajric@stu.ibu.edu.ba>
2025-11-17 06:08:07 +0100

    Milestone 3: CRUD, routes, OpenAPI.

---
+++ backend/rest/services/AirportService.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/BaseService.php';
+require_once __DIR__ . '/../dao/AirportDao.php';
+
+class AirportService extends BaseService {
+
+    public function __construct() {
+        parent::__construct(new AirportDao());
+    }
+}
---
+++ backend/rest/services/PaymentService.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/BaseService.php';
+require_once __DIR__ . '/../dao/PaymentDao.php';
+
+class PaymentService extends BaseService {
+
+    public function __construct() {
+        parent::__construct(new PaymentDao());
+    }
+}
