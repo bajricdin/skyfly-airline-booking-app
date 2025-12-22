85072c7696e502438f19ccda3b6a2333f700d29c
Din Bajric <din.bajric@stu.ibu.edu.ba>
2025-12-22 22:06:27 +0100

    Add files via upload

--- backend/rest/dao/config.php
+++ backend/rest/dao/config.php
@@ -1,34 +1,44 @@
 <?php
-// Set the reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
 class Config
 {
-   public static function DB_NAME()
-   {
-       return 'skyfly'; 
-   }
-   public static function DB_PORT()
-   {
-       return  3306;
-   }
-   public static function DB_USER()
-   {
-       return 'root';
-   }
-   public static function DB_PASSWORD()
-   {
-       return 'root';
-   }
-   public static function DB_HOST()
-   {
-       return 'localhost';
-   }
+    private static function env($key, $default = null)
+    {
+        return isset($_ENV[$key]) && $_ENV[$key] !== ''
+            ? $_ENV[$key]
+            : $default;
+    }
 
+    public static function DB_NAME()
+    {
+        return self::env('DB_NAME', 'skyfly');
+    }
 
-   public static function JWT_SECRET() {
-       return '8f3K!qPzA9$L2xM@YvW7dC0R#EJmS^hN6B';
-   }
+    public static function DB_PORT()
+    {
+        return self::env('DB_PORT', 3306);
+    }
+
+    public static function DB_USER()
+    {
+        return self::env('DB_USER', 'root');
+    }
+
+    public static function DB_PASSWORD()
+    {
+        return self::env('DB_PASSWORD', 'root');
+    }
+
+    public static function DB_HOST()
+    {
+        return self::env('DB_HOST', '127.0.0.1');
+    }
+
+    public static function JWT_SECRET()
+    {
+        return self::env('JWT_SECRET', '8f3K!qPzA9$L2xM@YvW7dC0R#EJmS^hN6B');
+    }
 }
